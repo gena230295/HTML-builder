1d5742bbeac1ba078ddd82241b6d61375fb18cea
Gena230295 <gena23solov@gmail.com>
2025-01-18 09:04:28 +0300

    feat slove first part '01-read-file'

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const textPath = path.join(__dirname, 'text.txt');
+
+const needFile = fs.createReadStream(textPath);
+needFile.on('data', (readText) => {
+  const arr = [];
+  for (let i = 0; i < readText.length; i++) {
+    arr.push(String.fromCharCode(readText[i]));
+  }
+  console.log(arr.join(''));
+});
